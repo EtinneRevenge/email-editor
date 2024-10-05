7d7b1653d10971af3abc5b8d2b8c9d7d454320a0
EtinneRevenge <blagostep550@gmail.com>
2024-10-05 23:20:04 +0300

    update styles

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,15 +1,9 @@
 import { EmailEditor } from "../../components/email-editor/EmailEditor";
 import { EmailList } from "../../components/email-list/EmailList";
+import styles from "./Home.module.scss"
 
 export function Home() {
-    return <div style={{
-        display: "grid",
-        gridTemplateColumns: '1fr .6fr',
-        padding: '1.5rem',
-        maxWidth: '1000px',
-
-
-    }}>
+    return <div className={styles.container}>
         <EmailEditor />
         <EmailList />
     </div>
